2a6d4418768959d7a959802301289605b5a3ae43
Mirosław Całka <m.calka@zst.kolbuszowa.pl>
2022-02-07 07:28:40 +0100

    Podstawy C++

--- Obiektowy/Podstawy/odleglosc_2.cpp
+++ Obiektowy/Podstawy/odleglosc_2.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-class punkt
+class point
 {
 private:
     double x, y;
@@ -14,7 +14,7 @@ public:
         cout << "y = ";
         cin >> y;
     }
-    double distance(punkt P)
+    double distance(point P)
     {
         return sqrt((P.x - x) * (P.x - x) + (P.y - y) * (P.y - y));
     }
@@ -22,7 +22,7 @@ public:
 
 int main()
 {
-    punkt A, B;
+    point A, B;
     A.read();
     B.read();
     cout << "Odleglosc = " << A.distance(B);
